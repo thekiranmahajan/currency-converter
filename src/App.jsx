8b5681d16122c8c765dd9c284b5f43cfa2512744
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-10-03 12:52:37 +0530

    components are ready to be used

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import InputBox from "./components/InputBox";
+import { InputBox } from "./components";
 
 const App = () => {
   return (
@@ -7,16 +7,26 @@ const App = () => {
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-xs bg-white/30">
           <form onSubmit={(e) => e.preventDefault()}>
             <div className="w-full mb-1">
-              <InputBox />
+              <InputBox label="From" />
             </div>
             <div>
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                className="absolute text-center left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 flex items-center justify-center text-white px-2 py-2"
               >
+                <span className="material-symbols-outlined">swap_vert</span>
                 Swap
               </button>
             </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox label="To" />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-4 rounded-lg"
+            >
+              Convert to
+            </button>
           </form>
         </div>
       </div>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,20 +1,37 @@
-const InputBox = () => {
+import { useId } from "react";
+
+const InputBox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) => {
+  const amountInputId = useId();
   return (
-    <div className="bg-white p-3 rounded-lg text-sm flex ">
+    <div className={` bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2 flex flex-col">
         <label
-          htmlFor="currencyName"
+          htmlFor={amountInputId}
           className="text-black/40 mb-2 inline-block"
         >
-          label
+          {label}
         </label>
         <input
+          id={amountInputId}
           type="number"
-          className="outline-none bg-transparent py-1.5
-          "
+          className="outline-none bg-transparent py-1.5"
           placeholder="Amount"
           min={0}
-         
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
@@ -22,8 +39,15 @@ const InputBox = () => {
         <select
           name="Currency Options"
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
         >
-          <option value="usd">usd</option>
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
         </select>
       </div>
     </div>
